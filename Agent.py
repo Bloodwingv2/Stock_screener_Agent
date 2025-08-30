7d7fcb232ea546fcdf4a0058cde3654b017ee926
Bloodwingv2 <bhandariumesh81@gmail.com>
2025-08-30 14:56:33 +0530

    Add Rich library and some colors for existing terminal CLI

--- Agent.py
+++ Agent.py
@@ -1,6 +1,7 @@
 from typing import Annotated, TypedDict
 from langgraph.graph import START, END, StateGraph
 from langgraph.graph.message import add_messages
+from langchain_core.messages import SystemMessage
 
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain.chat_models import init_chat_model
@@ -64,7 +65,7 @@ if __name__ == "__main__":
     while True:
         user_input = input("Pass your prompt Here:")
         if "exit" in user_input.lower(): # exit clause for our agent
-            print("\nExiting... Agent")
+            print(Fore.LIGHTYELLOW_EX + "\n Exiting... Agent" + Fore.RESET)
             break
         res = graph.invoke({"messages": [{"role": "user", "content": user_input}]}, config = {"configurable":{"thread_id":1234}})
-        print(Fore.LIGHTYELLOW_EX + res["messages"][-1].content + Fore.RESET) # colour the Agent CMD
+        print(Fore.GREEN + res["messages"][-1].content + Fore.RESET) # colour the Agent CMD
